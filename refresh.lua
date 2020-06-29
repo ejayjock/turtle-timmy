df9f44901a97537b7240f0324f959e9fe0e9a5ea
Eric Jayjock <ejayjock@gmail.com>
2020-06-29 15:36:28 -0400

    Created refresh script to run new github sync program.

---
+++ refresh.lua
@@ -0,0 +1,7 @@
+--[[ Script cleans all local files and syncs with github repository]]--
+
+local turtleName = "timmy"
+local gUser = "ejayjock"
+
+github clone ejayjock/turtle-timmy -a ejayjock
+shell.run("cd turtle-timmy")
