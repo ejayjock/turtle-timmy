15f5dc38b8492be0b4b53397be555020158cfde0
Eric Jayjock <ejayjock@gmail.com>
2020-06-29 15:51:21 -0400

    Added startup script to make sure turtles start off in proper directory.

--- refresh.lua
+++ refresh.lua
@@ -3,5 +3,7 @@
 local turtleName = "timmy"
 local gUser = "ejayjock"
 
-github clone ejayjock/turtle-timmy -a ejayjock
+/github.rom/program/github clone ejayjock/turtle-timmy -a ejayjock
+shell.run("rm /startup.lua")
+shell.run("mv turtle-timmy/startup.lua /startup.lua")
 shell.run("cd turtle-timmy")
---
+++ startup.lua
@@ -0,0 +1,4 @@
+local tName="timmy"
+print(tName.." the turtle at your service!")
+print("Type refresh anytime to sync me with gitHub")
+shell.run("cd turtle-"..tName.."/")
