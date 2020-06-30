32f2cf8d04e9c9f58307600307881aa2b7bcfd14
Eric Jayjock <ejayjock@gmail.com>
2020-06-30 14:12:57 -0400

    Included a line fully delete all local files.

--- refresh.lua
+++ refresh.lua
@@ -1,9 +1,9 @@
---[[ ]]--
+-- Code to clean out existing turtle directory and clone the git repository to this local directory. NOTE: This will delete any local files!
 
 local tName = "timmy"
 local gitRunCmd="github"
 local gitArgs=" clone ejayjock/turtle-"..tName
 
 shell.run('cd /')
---shell.run('rm turtle-'..tName)
+shell.run('rm turtle-'..tName)
 shell.run(gitRunCmd..gitArgs)
