afaaf0e8500c8f38e08fb6898423f27b84df7209
Eric Jayjock <ejayjock@gmail.com>
2020-06-30 14:09:55 -0400

    Updated refresh to use Eric Wiesers github approach.

--- refresh.lua
+++ refresh.lua
@@ -1,12 +1,9 @@
---[[ Rev 03 Script cleans all local files and syncs with github repository]]--
+--[[ ]]--
 
-local turtleName = "timmy"
-local gUser = "ejayjock"
+local tName = "timmy"
+local gitRunCmd="github"
+local gitArgs=" clone ejayjock/turtle-"..tName
 
-local gitRunCmd="/github.rom/program/github"
-local gitArgs=" clone ejayjock/turtle-"..turtleName.." -a "..gUser
-
+shell.run('cd /')
+--shell.run('rm turtle-'..tName)
 shell.run(gitRunCmd..gitArgs)
-
---shell.run("rm /startup.lua")
---shell.run("mv turtle-timmy/startup.lua /startup.lua")
