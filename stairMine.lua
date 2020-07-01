f7966bb9007e09d1d5ac09e779a334086ae889df
Eric Jayjock <ejayjock@gmail.com>
2020-07-01 10:25:58 -0400

    started stairmine.lua

---
+++ stairMine.lua
@@ -0,0 +1,10 @@
+--[[ Code to be repeated to create a staircase mine
+Series of turtle commands:
+  - Dig Up
+  - Move Up
+  - Dig Up
+  - Move down
+  - dig down
+  - move down
+  - dig Forward
+  - move forward ]]
