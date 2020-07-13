8962d114a6bfa8b4bd81b82b303e095295798661
Eric Jayjock <ejayjock@gmail.com>
2020-07-13 13:42:29 -0400

    added loop to dig down 5 spaces.

--- stairMine.lua
+++ stairMine.lua
@@ -8,3 +8,14 @@ Series of turtle commands:
   - move down
   - dig Forward
   - move forward ]]
+
+  for i=1,5 do
+    turtle.digUp()
+    turtle.up()
+    turtle.digUp()
+    turtle.down()
+    turtle.digDown()
+    turtle.down()
+    turtle.dig()
+    turtle.forward()
+  end
