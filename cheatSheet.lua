6cda8ab8619daa7c45dd37d4d7ff98a1b2a6a953
Eric Jayjock <ejayjock@gmail.com>
2020-07-15 14:21:17 -0400

    UPdated cheat sheet

--- cheatSheet.lua
+++ cheatSheet.lua
@@ -1,3 +1,6 @@
+
+
+
 if turtle.detect() then
   turtle.dig()
 end
@@ -5,9 +8,79 @@ end
 
 cont=true
 while cont do
-  if --[Test Something--]] then
-    -- Commands
+  if turtle.detectUp() then
+    turtle.digUp()
   else
     cont=false
   end
+end
+
+-- Other commands:
+turtle.detectUp()
+turtle.detectDown()
+turtle.digUp()
+turtle.digDown()
+
+
+
+for i=1,5 do
+    --turtle.digUp()
+    cont=true
+    while cont do
+      if turtle.detectUp() then
+        turtle.digUp()
+      else
+        cont=false
+      end
+    end
+
+    turtle.up()
+    --turtle.digUp()
+    turtle.down()
+    turtle.digDown()
+    turtle.down()
+    --turtle.dig()
+    turtle.forward()
+end
+
+
+
+
+
+for i=1,5 do
+  --turtle.digUp()
+  cont=true
+  while cont do
+    if turtle.detectUp() then
+      turtle.digUp()
+    else
+      cont=false
+    end
+  end
+  turtle.up()
+
+  -- turtle.digUp()
+  cont=true
+  while cont do
+    if turtle.detectUp() then
+      turtle.digUp()
+    else
+      cont=false
+    end
+  end
+
+  turtle.down()
+  turtle.digDown()
+  turtle.down()
+
+  -- turtle.dig()
+  cont=true
+  while cont do
+    if turtle.detect() then
+      turtle.dig()
+    else
+      cont=false
+    end
+  end
+  turtle.forward()
 end
