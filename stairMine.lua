b70164cc5f9772c9cbcf21356a993a065d3d2bfc
Eric Jayjock <ejayjock@gmail.com>
2020-07-17 11:07:05 -0400

    Added a general function with input for direction.

--- stairMine.lua
+++ stairMine.lua
@@ -10,28 +10,63 @@ Series of turtle commands:
   - move forward ]]
 
   -- Utility Functions
-  function clearUp()
-    cont=true
-    while cont do
-      if turtle.detectUp() then
-        turtle.digUp()
-      else
-        cont=false
-      end
+function clearUp()
+  cont=true
+  while cont do
+    if turtle.detectUp() then
+      turtle.digUp()
+    else
+      cont=false
     end
   end
+end
 
-  function clear()
-    cont=true
-    while cont do
+function clearForward()
+  cont=true
+  while cont do
+    if turtle.detect() then
+      turtle.dig()
+    else
+      cont=false
+    end
+  end
+end
+
+function clear(direction)
+  args={...}
+
+  cont=true
+  if args[1] == nil or args[1] == 'forward' then
+    -- Loop to interate until space in front of turtle is clear
+    while cont
       if turtle.detect() then
         turtle.dig()
       else
         cont=false
       end
     end
+  elseif args[1] == 'up' then
+    -- Loop to interate until space in above turtle is clear
+    while cont
+      if turtle.detectUp() then
+        turtle.digUp()
+      else
+        cont=false
+      end
+    end
+  elseif args[1] == 'down' then
+    -- Loop to interate until space in below turtle is clear
+    while cont
+      if turtle.detectDown() then
+        turtle.digDown()
+      else
+        cont=false
+      end
+    end
+  else
+    error(usageStatment)
   end
-
+end
 
 for i=1,5 do
   clearUp()
